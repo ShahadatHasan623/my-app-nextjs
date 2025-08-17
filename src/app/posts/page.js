50d2d78800a14fad178b0829153cf4b30ed007e5
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-08-17 20:23:57 +0600

    added css styling

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import style from "./post.module.css"
 
 export const getPost = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -15,8 +16,8 @@ export default async function Posts() {
         return (
           <>
             <div key={p.id} className="border-2 border-slate-50 p-4">
-              <p className="text-2xl font-extrabold space-y-1 mb-4">{p.title}</p>
-              <p>{p.body}</p>
+              <p className={`text-2xl font-extrabold space-y-1 mb-4 ${style["post-title"]}`}>{p.title}</p>
+              <p className="testing-purpose-css">{p.body}</p>
               <Link href={`posts/${p.id}`}>
                 <button className="bg-green-400 px-4 py-2 text-black rounded-xl hover:bg-green-800">Details</button>
               </Link>
